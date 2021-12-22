b297011583a748a9fd0cfcf76164cc6feeae4a9e
Sevi <correosevi@gmail.com>
2021-12-22 16:51:50 +0000

    Test for the Kata completed

--- main.js
+++ main.js
@@ -2,4 +2,17 @@
 👉 Write your kata here!
 */
 
+//PLAN
+// 1. Create a test that check the function
+//  The test should fail fist time
+//  Return a fake result on the function
+//  Then we create the actual app.
+// 2. Create the main function for returning the highest and the lowest number
+// from an string.
+// 3. Improve the app (to decide)
+// 4. Write the kata
+
 //👉 Write the function your CodeWarriors will start with below here:
+export function highAndLow(string) {
+  return [min, max];
+}
